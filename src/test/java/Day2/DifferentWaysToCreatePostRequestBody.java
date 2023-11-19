2621c7393a470ba2bd1b68ba46fd53252b52e6a8
Ashfat <ashfat.rashid@polygontech.xyz>
2023-11-19 13:45:12 +0600

    Learning Rest Assured API Automation

--- src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
+++ src/test/java/Day2/DifferentWaysToCreatePostRequestBody.java
@@ -1,14 +1,22 @@
 package Day2;
 
+import org.json.JSONObject;
+import org.json.JSONTokener;
 import org.testng.annotations.Test;
+
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileReader;
 import java.util.HashMap;
 
 public class DifferentWaysToCreatePostRequestBody {
-     
-    @Test(priority = 1)
+    
+    //Post Request Body Using HashMap()
+
+    //@Test(priority = 1)
     void testPostUsingHashMap(){
 
        HashMap data = new HashMap();
@@ -17,8 +25,8 @@ public class DifferentWaysToCreatePostRequestBody {
        data.put("location","France");
        data.put("phone","0123456");
 
-       String courseArr[] =  {"C","C++"};
-       data.put("course","courseArr");
+       String coursesArr[] =  {"C","C++"};
+       data.put("courses",coursesArr);
 
        given()
            .contentType("application/json")
@@ -31,7 +39,7 @@ public class DifferentWaysToCreatePostRequestBody {
              .statusCode(201)
              .body("name",equalTo("Nadim"))
              .body("location",equalTo("France"))
-             .body("phone",equalTo("0123456"))
+             .body("phone",equalTo("123456"))
              .body("courses[0]",equalTo("C"))
              .body("courses[1]",equalTo("C++"))
 
@@ -39,6 +47,95 @@ public class DifferentWaysToCreatePostRequestBody {
     
              .log().all();
     }   
+
+    //Post Request Body Using org.json library
+
+    //@Test(priority = 1)
+    void testPostUsingJsonLibrary() {
+
+        JSONObject obj = new JSONObject();
+        obj.put("name", "Scott");
+        obj.put("location", "France");
+        obj.put("phone", "123456");
+    
+        String coursesArr[] = {"C", "C++"};
+        obj.put("courses", coursesArr);
+    
+        given()
+                .contentType("application/json")
+                .body(obj.toString()) // -> toString() method is only required for json library
+            .when()
+                .post("https://localhost:3000/students")
+            .then()
+                .statusCode(201)
+                .body("name", equalTo("Nadim")) 
+                .body("location", equalTo("France"))
+                .body("phone", equalTo("123456")) 
+                .body("courses[0]", equalTo("C"))
+                .body("courses[1]", equalTo("C++"))
+                .header("Content-Type", "application/json; charset=utf-8")
+               
+                .log().all();
+    }
+
+    //Post Request Body Using POJO Class -> Plain Old Java Object
+
+    //@Test(priority = 1)
+    void testPostUsingPOJO() {
+
+       POJO_PostRequest data = new POJO_PostRequest();
+
+       data.setName("Nadim");
+       data.setLocation("France");
+       data.setPhone("123456");
+
+       String coursesArr[] = {"C","C++"};
+       data.setCourses(coursesArr);
+    
+        given()
+                .contentType("application/json")
+                .body(data)
+            .when()
+                .post("https://localhost:3000/students")
+            .then()
+                .statusCode(201)
+                .body("name", equalTo("Nadim")) 
+                .body("location", equalTo("France"))
+                .body("phone", equalTo("123456")) 
+                .body("courses[0]", equalTo("C"))
+                .body("courses[1]", equalTo("C++"))
+                .header("Content-Type", "application/json; charset=utf-8")
+                .log().all();
+    }
+
+    //Post Request Body Using External JSON File
+
+    @Test(priority = 1)
+    void testPostUsingExternalJsonFile() throws FileNotFoundException {
+
+        File f = new File(".\\Body.json");
+
+        FileReader fr = new FileReader(f);
+
+        JSONTokener jt = new JSONTokener(fr);
+
+        JSONObject data = new JSONObject(jt);
+    
+        given()
+                .contentType("application/json")
+                .body(data)
+            .when()
+                .post("https://localhost:3000/students")
+            .then()
+                .statusCode(201)
+                .body("name", equalTo("Nadim")) 
+                .body("location", equalTo("France"))
+                .body("phone", equalTo("123456")) 
+                .body("courses[0]", equalTo("C"))
+                .body("courses[1]", equalTo("C++"))
+                .header("Content-Type", "application/json; charset=utf-8")
+                .log().all();
+    }
     
 
     @Test(priority = 2)
