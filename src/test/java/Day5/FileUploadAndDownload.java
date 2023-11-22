362632e5ffaa4701908e1e53299d1a0060b1f9be
Ashfat <ashfat.rashid@polygontech.xyz>
2023-11-22 16:40:41 +0600

    Learning Rest Assured API Automation

--- src/test/java/Day5/FileUploadAndDownload.java
+++ src/test/java/Day5/FileUploadAndDownload.java
@@ -9,41 +9,59 @@ import org.testng.annotations.Test;
 
 public class FileUploadAndDownload {
 
-    // @Test
-    // void singleFileUpload() {
+    @Test(priority = 1)
+    void singleFileUpload() {
 
-    //     File myfile = new File("/home/ashfatrashid/Downloads/Body.json");
+        File myfile = new File("/home/ashfatrashid/Downloads/Body.json");
 
-    //     given()
-    //         .multiPart("file", myfile) //Equivalent to form-data with post request body in postman 
-    //         .contentType("multipart/form-data")
-
-    //     .when()
-    //         .post("http://localhost:8080/uploadFile")
-
-    //     .then()
-    //         .statusCode(200)
-    //         .body("filename", equalTo("Body.json"));
-    // }
-
-
-    @Test
-    void multipleFilesUpload() {
-
-        File myfile1 = new File("/home/ashfatrashid/Downloads/Body.json");
-        File myfile2 = new File("/home/ashfatrashid/Downloads/Body.json");
-       
         given()
-            .multiPart("files", myfile1) //Equivalent to form-data with post request body in postman 
-            .multiPart("files", myfile2) 
+            .multiPart("file", myfile) //Equivalent to form-data with post request body in postman 
             .contentType("multipart/form-data")
 
         .when()
-            .post("http://localhost:8080/uploadMultipleFiles")
+            .post("http://localhost:8080/uploadFile")
 
         .then()
             .statusCode(200)
-            .body("[0].filename", equalTo("Body.json"))
-            .body("[1].filename", equalTo("Body2.json"));
+            .body("filename", equalTo("Body.json"));
+    }
+
+
+    // @Test
+    // void multipleFilesUpload() {
+
+    //     File myfile1 = new File("/home/ashfatrashid/Downloads/Body.json");
+    //     File myfile2 = new File("/home/ashfatrashid/Downloads/Body.json");
+       
+    //     //Array Apprtoach for storing files but won't work for all typos of API
+    //     //File filearr[] = {myfile1, myfile2};
+
+    //     given()
+    //         .multiPart("files", myfile1) //Equivalent to form-data with post request body in postman 
+    //         .multiPart("files", myfile2) 
+
+    //         //.multiPart("files", filearr) -> Array Approach
+    //         .contentType("multipart/form-data")
+
+    //     .when()
+    //         .post("http://localhost:8080/uploadMultipleFiles")
+
+    //     .then()
+    //         .statusCode(200)
+    //         .body("[0].filename", equalTo("Body1.json"))
+    //         .body("[1].filename", equalTo("Body2.json"));
+    // }
+
+    @Test(priority = 2)
+    void fileDownload(){
+        
+        given()
+
+        .when()
+           .get("http://localhost:8080/downloadFile/Body1.json") //-> For Test1
+
+        .then()
+           .statusCode(200)
+           .log().body();
     }
 }
