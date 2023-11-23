363c050dfd6cfb79ca4a35b6275d6f67b3d8ee96
Ashfat <ashfat.rashid@polygontech.xyz>
2023-11-23 18:54:47 +0600

    Learning Rest Assured API Automation

--- src/test/java/Day6/JSONSChemaValidation.java
+++ src/test/java/Day6/JSONSChemaValidation.java
@@ -1,22 +1,23 @@
 package Day6;
 
-import static io.restassured.RestAssured.when;
-import static io.restassured.RestAssured.given;
-import static org.hamcrest.Matchers.*;
-
 import org.testng.annotations.Test;
+
+import static io.restassured.RestAssured.given;
+import static io.restassured.RestAssured.when;
+import static io.restassured.module.jsv.JsonSchemaValidator.matchesJsonSchemaInClasspath;
 
 public class JSONSChemaValidation {
 
     @Test
-    void JSONschemaValidation(){
-       
-        .given()
+    void JSONschemaValidation() {
+
+        given()
 
         .when()
            .get("https://jsonplaceholder.typicode.com/users")
 
         .then()
-            .assertThat().body(JSONSChemaValidator.MatchersJSONSchemaInCLasspath("src/test/resources/JSONSchema/JSONSchema.json"));
+            .assertThat().body(matchesJsonSchemaInClasspath("src/test/resources/JSONSchema/JSONSchema.json"));
     }
 }
+
