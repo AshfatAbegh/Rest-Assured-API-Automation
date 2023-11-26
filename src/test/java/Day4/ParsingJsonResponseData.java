872f375885dc7a7530f357c40c32c00338cae1ba
Ashfat <ashfat.rashid@polygontech.xyz>
2023-11-26 13:29:04 +0600

    Learning Rest Assured API Automation

--- src/test/java/Day4/ParsingJsonResponseData.java
+++ src/test/java/Day4/ParsingJsonResponseData.java
@@ -3,6 +3,7 @@ package Day4;
 import static io.restassured.RestAssured.*;
 import static org.hamcrest.Matchers.*;
 
+import org.json.JSONArray;
 import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
@@ -10,55 +11,54 @@ import org.testng.annotations.Test;
 import io.restassured.response.Response;
 
 public class ParsingJsonResponseData {
-    
-    @Test
-    void testJsonResponse(){
-         
-        // Approach 1 -> Direct Validation on then() Reponse
 
-        // given()
-        //     .contentType("ContentType.JSON")
-        // .when()
-        //     .get("https://jsonplaceholder.typicode.com/posts")
-        // .then()
-        //     .statusCode(200)
-        //     .header("Content-Type","application/json; charset=utf-8")
-        //     .body("[4].title",equalTo("nesciunt quas odio"));
-        
+   @Test
+   void testJsonResponse() {
 
-        // Approach 2 -> If we want to perform validation on a seperate response variable and then validate the response
+      // Approach 1 -> Direct Validation on then() Reponse
 
-        Response res = given()
-           .contentType("application/json; charset=utf-8")
+      // given()
+      // .contentType(ContentType.JSON)
+      // .when()
+      // .get("https://jsonplaceholder.typicode.com/posts")
+      // .then()
+      // .statusCode(200)
+      // .header("Content-Type","application/json; charset=utf-8")
+      // .body("[4].title",equalTo("nesciunt quas odio"));
 
-        .when()
-           .get("https://jsonplaceholder.typicode.com/posts");
-        
-        Assert.assertEquals(res.getStatusCode(), 200); // Validation 1 ->Assert.assertEquals is a TestNG Annotation 
-        Assert.assertEquals(res.header("Content-Type"),"application/json; charset=utf-8"); //Validation 2
-        
-        String title = res.jsonPath().get("[4].title");
-        Assert.assertEquals(title, "nesciunt quas odio");
-    
-    
-        //JSON Object Class
-        //Search for title of the user in json -> Validation
+      // Approach 2 -> If we want to perform validation on a seperate response
+      // variable and then validate the response
 
-        boolean status = false;
+      Response res = given()
+            .contentType("application/json; charset=utf-8")
 
-         JSONObject jo = new JSONObject(res.toString()); //converting response to json object type
-        
-         for(int i=0; i<jo.length(); i++){
-             String newTitle = jo.getJSONArray(title).getJSONObject(i).get("title").toString();
-             System.out.println(newTitle);
+            .when()
+            .get("https://jsonplaceholder.typicode.com/posts");
 
-             if(newTitle.equals("nesciunt quas odio")){
-                status = true;
-                break;
-             }
+      Assert.assertEquals(res.getStatusCode(), 200); // Validation 1 ->Assert.assertEquals is a TestNG Annotation
+      Assert.assertEquals(res.header("Content-Type"), "application/json; charset=utf-8"); // Validation 2
+
+      String title = res.jsonPath().get("[4].title");
+      Assert.assertEquals(title, "nesciunt quas odio");
+
+      // JSON Object Class
+      // Search for title of the user in json -> Validation
+
+      boolean status = false;
+
+      String responseBody = res.getBody().asString();
+      JSONArray ja = new JSONArray(responseBody);
+
+      for (int i = 0; i < ja.length(); i++) {
+         String newTitle = ja.getJSONObject(i).getString("title");
+         System.out.println(newTitle);
+
+         if (newTitle.equals("nesciunt quas odio")) {
+            status = true;
+            break;
          }
+      }
 
-         Assert.assertEquals(status, true);
+      Assert.assertEquals(status, true);
    }
 }
-
